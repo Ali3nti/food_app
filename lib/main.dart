f471e36f31b6ca94fb3130065e3e29b822bc3da5
Ali3nti <Ali3nti@gmail.com>
2021-02-17 09:55:41 +0330

    vs tested & ok

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Food App 2',
+      title: 'Food App',
       theme: ThemeData(
         accentColor: kPrimaryColor,
         scaffoldBackgroundColor: kBackgroundColor,
